39fee36c1f61dbeef31f32059567d0c7a2a0f767
Andrew Yoon <sky417@gmail.com>
2021-09-13 09:07:04 -0400

    added keycode-string map, should I use char...

---
+++ keyString.h
@@ -0,0 +1,39 @@
+/**
+ * 
+ * keyString.h
+ * 
+ * Contains key:string unordered_map
+ */
+
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+unordered_map<int, string> keyStrMap = {
+    /* Special keys */
+    {0x8, "Backspace"}, {0xd, "Enter"}, {0x23, "End"}, {0x24, "Home"},
+    {0x1b, "ESC"}, {0x20, "Space"},  {0x21, "PgUp"}, {0x22, "PgDn"}, {0x2e, "Del"},
+    /* Arrows */
+    {0x25, "LeftArrow"}, {0x26, "UpArrow"}, {0x27, "RightArrow"}, {0x28, "DownArrow"},
+    /* Numbers and alphabets */
+    {0x30, "0"}, {0x31, "1"}, {0x32, "2"}, {0x33, "3"}, {0x34, "4"},
+    {0x35, "5"}, {0x36, "6"}, {0x37, "7"}, {0x38, "8"}, {0x39, "9"},
+    {0x41, "A"}, {0x42, "B"}, {0x43, "C"}, {0x44, "D"}, {0x45, "E"},
+    {0x46, "F"}, {0x47, "G"}, {0x48, "H"}, {0x49, "I"}, {0x4a, "J"},
+    {0x4b, "K"}, {0x4c, "L"}, {0x4d, "M"}, {0x4e, "N"}, {0x4f, "O"},
+    {0x50, "P"}, {0x51, "Q"}, {0x52, "R"}, {0x53, "S"}, {0x54, "T"},
+    {0x55, "U"}, {0x56, "V"}, {0x57, "W"}, {0x58, "X"}, {0x59, "Y"},
+    {0x5a, "Z"},
+    /* F-row */
+    {0x70, "F1"}, {0x71, "F2"}, {0x72, "F3"}, {0x73, "F4"},
+    {0x74, "F5"}, {0x75, "F6"}, {0x76, "F7"}, {0x77, "F8"},
+    {0x78, "F9"}, {0x79, "F10"}, {0x7a, "F11"}, {0x7b, "F12"},
+    /* R1/R2 keys */
+    {0x15, "RAlt"}, {0x19, "RCtrl"}, {0xa1, "RShift"},
+    {0xa0, "LShift"}, {0xa2, "LCtrl"}, {0xa4, "LAlt"}, {0x5b, "LWinKey"},
+    /* Special characters */
+    {0xba, ";"}, {0xbb, "="}, {0xbc, ","}, {0xbd, "-"}, {0xbe, "."},
+    {0xbf, "/"}, {0xc0, "`"}, {0xdb, "["}, {0xdc, "\\"}, {0xdd, "]"},
+    {0xde, "'"},
+};
