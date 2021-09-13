ed6df442297c602aa7f5cb2a3d16e52664c3db15
Andrew Yoon <sky417@gmail.com>
2021-09-12 22:04:26 -0400

    initial commit, nothing yet

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * 
+ * main.cpp
+ * 
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(void) {
+
+    cout << "Hello world" << endl;
+
+    return 0;
+}
