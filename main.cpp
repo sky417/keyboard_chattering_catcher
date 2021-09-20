49ed1a7baa1bc8df67aeb12ba7e813f725fc7862
Andrew Yoon <sky417@gmail.com>
2021-09-20 00:49:26 -0400

    replaced epoch time with stringified..

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include <chrono>
 #include <windows.h>
 #include <stdio.h>
+#include <sstream>
+#include <iomanip>
 #include "keyString.h"
 
 using namespace std;
@@ -34,6 +36,18 @@ bool has_key(unordered_map<int, string> m, char key) {
 
     return true;
 }
+
+using time_point = std::chrono::system_clock::time_point;
+std::string serializeTimePoint( const time_point& time, const std::string& format)
+{
+    std::time_t tt = std::chrono::system_clock::to_time_t(time);
+    //std::tm tm = *std::gmtime(&tt); //GMT (UTC)
+    std::tm tm = *std::localtime(&tt); //Locale time-zone, usually UTC by default.
+    std::stringstream ss;
+    ss << std::put_time( &tm, format.c_str() );
+    return ss.str();
+}
+
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 
     if (nCode == HC_ACTION)
@@ -56,7 +70,8 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
                 }
 
                 if ((now - lastTs) < threshold) {
-                    cout << now << ":: CHATTERING << " <<  keyStrMap[p->vkCode] << " >>, delta "  << (now - lastTs) << endl;
+                    cout << serializeTimePoint(chrono::system_clock::now(), "%Y-%m-%d %H:%M:%S")
+                         << " ---- CHATTERING << " <<  keyStrMap[p->vkCode] << " >>, delta "  << (now - lastTs) << endl;
                     return 1;
                 }
             }
